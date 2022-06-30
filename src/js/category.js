43e3d95da4c567a1ac0c05c4193e361216a011bd
Matin-Sangabi <89559512+Matin-Sangabi@users.noreply.github.com>
2022-06-30 16:10:33 +0430

    category was updated

--- src/js/category.js
+++ src/js/category.js
@@ -1,13 +1,29 @@
 import Storage from "./storage.js";
 const categoryParent = document.querySelector(".category")
-const categoryTitle = document.querySelector(".cat-title");
-const categoryDescription = document.querySelector(".cat-description");
-const addCategory = document.querySelector(".add-category");
-const newCategory = document.querySelector(".add-new-category");
-
 
 export default class Category {
     constructor(){
+        this.createCategory();  
+    }
+    createCategory(){
+        const categoryDiv = document.createElement("div");
+        categoryDiv.className = "flex flex-col gap-y-4 bg-slate-700 rounded-lg p-2";
+        categoryDiv.innerHTML = `
+        <div class="category-title flex flex-col gap-y-2 w-1/2">
+            <span>Title</span>
+            <input type="text" placeholder="Title" class="cat-title form-input bg-transparent rounded-md ">
+        </div>
+        <div class="category-description flex flex-col gap-y-2">
+            <span>Description</span>
+            <textarea class="cat-description form-textarea bg-transparent rounded-md resize-none" placeholder="Description ..."></textarea>
+        </div>
+        <button type="button" class="add-category bg-slate-500 border-none outline-none  rounded-md p-2 hover:border-none hover:outline-none hover:ring  hover:ring-offset-2 hover:ring-slate-500">Add new Category</button>`;
+        categoryParent.appendChild(categoryDiv);
+        
+         this.categoryTitle = document.querySelector(".cat-title");
+        this.categoryDescription = document.querySelector(".cat-description");
+        const addCategory = document.querySelector(".add-category");
+        const newCategory = document.querySelector(".add-new-category");
         newCategory.addEventListener("click" , ()=>this.openCategory());
         addCategory.addEventListener("click" , (e)=> this.addNewCategory(e));
     }
@@ -15,14 +31,14 @@ export default class Category {
         categoryParent.classList.remove("hidden");
     }
     addNewCategory(e){
-        if(! categoryTitle.value && ! categoryDescription.value) return;
+        if(! this.categoryTitle.value && ! this.categoryDescription.value) return;
         const saveCategory = {
-            title : categoryTitle.value,
-            description : categoryDescription.value,
+            title : this.categoryTitle.value,
+            description : this.categoryDescription.value,
         }
         Storage.saveCategories(saveCategory);
-        categoryTitle.value = "";
-        categoryDescription.value = "";
+        this.categoryTitle.value = "";
+        this.categoryDescription.value = "";
         this.closeCategory();
     }
     closeCategory(){
